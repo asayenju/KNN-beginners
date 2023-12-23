74b4d0eca4bf71d85b94eebe5fbc2b0c6e9c3f68
ashwinsayenju@gmail.com <ashwinsayenju@gmail.com>
2023-12-22 16:39:18 -0800

    use the algorithm or KNN class to have results from the dataset

--- KNN.py
+++ KNN.py
@@ -1,16 +1,16 @@
 import numpy as np
 from collections import Counter
 def euclidean_distance(x1, x2):
-    distance = np.sqrt(np.sumz(x1-x2)**2) 
+    distance = np.sqrt(np.sum(x1-x2)**2) 
     return distance
 
 class KNN:
     def __init__(self, k=3):
         self.k = k
 
-    def fit(self, X, Y):
+    def fit(self, X, y):
         self.X_train = X
-        self.Y_train = Y
+        self.y_train = y
 
     def predict(self, X):
         predictions = [self._predict(x) for x in X]
@@ -25,4 +25,4 @@ class KNN:
 
         #majority voyage
         most_common = Counter(k_nearest_labels).most_common()
-        return most_common
+        return most_common[0][0]
